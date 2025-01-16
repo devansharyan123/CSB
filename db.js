f6e6c142642eb08c7a340066a769304754d6bf03
DEVANSH ARYAN <1ds22cb022@dsce.edu.in>
2025-01-16 21:22:20 +0530

    database added

---
+++ db.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const { number } = require('zod');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI);
+const userSchema = new Schema({
+    email : {type : String , unique : true},
+    password : String,
+    firstName : String,
+    lastName : String
+})
+
+const adminSchema = new Schema({
+    email : {type : String , unique : true},
+    password : String,
+    firstName : String,
+    lastName : String
+})
+
+const courseSchema = new Schema({
+   
+    title : String,
+    description : String,
+    imageUrl : String,
+    price : Number,
+    creatorId : ObjectId
+})
+
+const purchaseSchema = new Schema({
+    courseId : ObjectId,
+    userId : ObjectId
+})
+
+const userModel = mongoose.model("users" , userSchema)
+const adminModel = mongoose.model("admin" , adminSchema)
+const courseModel = mongoose.model("courses" , courseSchema)
+const purchaseModel = mongoose.model("purschase" , purchaseSchema)
+
+module.exports = {
+    userModel ,
+    adminModel ,
+    courseModel ,
+    purchaseModel
+}
