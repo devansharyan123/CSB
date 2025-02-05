480b967fb05d465aa5d3b544aa5d7ea3668635ba
DEVANSH ARYAN <1ds22cb022@dsce.edu.in>
2025-02-05 13:50:15 +0530

    all admin routes enabled

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const adminRouter = Router();
-const { adminModel } = require("../db");
+const { adminModel, courseModel } = require("../db");
 const jwt = require('jsonwebtoken');
 const adminMiddleware = require('../middleware/adminMiddleware');
+const course = require('./course');
 require('dotenv').config();
 const jwt_secretKey = process.env.jwt_secret_admin;
 
@@ -56,9 +57,14 @@ adminRouter.post('/signin', async function (req, res) {
 
 })
 
-adminRouter.get('/course/bulk', function (req, res) {
+adminRouter.get('/course/bulk',adminMiddleware, async function (req, res) {
+    const adminId = req.adminId;
+    const course = await courseModel.find({
+        creatorId : adminId
+    })
     res.json({
-        message: "SIGN UP"
+        message: "Course updated",
+        courseId : course._id
     })
 
 })
@@ -71,7 +77,7 @@ adminRouter.post('/course', adminMiddleware, function (req, res) {
             description,
             imageUrl, // watch creating a web 3 saas in 6 hrs by harkirat to learn how to upload a direct image in database without using url
             price,
-            creatorId: adminId
+            creatorId: req.adminId
         })
 
         res.json({
@@ -86,9 +92,23 @@ adminRouter.post('/course', adminMiddleware, function (req, res) {
 
 })
 
-adminRouter.put('/course', function (req, res) {
+adminRouter.put('/course', adminMiddleware , async function (req, res) {
+    const adminId = req.adminId;
+
+    const {title , description , imageUrl, price , courseId} = req.body
+
+    const course = await courseModel.updateOne({
+        _id : courseId,
+        creatorId : adminId
+    } ,{
+        title,
+        description,
+        price,
+        imageUrl
+    })
     res.json({
-        message: "SIGN UP"
+        message: "Course updated",
+        courseId : course._id
     })
 
 })
